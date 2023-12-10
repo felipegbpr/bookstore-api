aedd076c4ea43cddeb7f8d57120eee21943f4842
felipegbpr <felipetecacc@gmail.com>
2023-12-10 11:29:59 -0300

    DELETE para Categoria

--- src/main/java/com/felipe/bookstore/service/CategoriaService.java
+++ src/main/java/com/felipe/bookstore/service/CategoriaService.java
@@ -20,7 +20,7 @@ public class CategoriaService {
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + "Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 	public List<Categoria> findAll() {
@@ -37,6 +37,11 @@ public class CategoriaService {
 		obj.setNome(objDto.getNome());
 		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		repository.deleteById(id);
 	}
 
 }
