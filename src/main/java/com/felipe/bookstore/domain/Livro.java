35b45975d50ec38cb03322c103903754c92eb979
felipegbpr <felipetecacc@gmail.com>
2023-12-11 14:12:06 -0300

    Listando todos os livros por categoria

--- src/main/java/com/felipe/bookstore/domain/Livro.java
+++ src/main/java/com/felipe/bookstore/domain/Livro.java
@@ -23,7 +23,7 @@ public class Livro implements Serializable {
 	private String titulo;
 	private String nome_autor;
 	private String texto;
-	
+
 	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "categoria_id")
@@ -36,6 +36,26 @@ public class Livro implements Serializable {
 		this.nome_autor = nome_autor;
 		this.texto = texto;
 		this.categoria = categoria;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getNome_autor() {
+		return nome_autor;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
 	}
 
 	public void setId(Integer id) {
