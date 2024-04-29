cfbb6f95c3e488ccba1fcaf141fcdb9a29ba3588
Felipe Borges <felipetecacc@gmail.com>
2024-04-29 13:32:25 -0300

    correcao de palavras

--- src/main/java/com/felipe/bookstore/service/DBService.java
+++ src/main/java/com/felipe/bookstore/service/DBService.java
@@ -22,7 +22,7 @@ public class DBService {
 	public void instanciaBaseDeDados() {
 		
 		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
-		Categoria cat2 = new Categoria(null, "Ficcção Científica", "Ficcção científica");
+		Categoria cat2 = new Categoria(null, "Ficção Científica", "Ficção científica");
 		Categoria cat3 = new Categoria(null, "Biografias", "Livros de Biografias");
 				
 		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
